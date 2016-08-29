203cd9d6d347d84c3043c183d77ff5a39cb0d6d5
chris <chris@digitalworlds.ufl.edu>
2016-08-29 13:49:02 -0400

    TypeScript compiler output files added to version control

---
+++ shared/Endpoint.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chris on 8/17/16.
+ */
+var Endpoint = (function () {
+    function Endpoint() {
+        this.foo = 4;
+    }
+    return Endpoint;
+})();
+exports.__esModule = true;
+exports["default"] = Endpoint;
+//# sourceMappingURL=Endpoint.js.map

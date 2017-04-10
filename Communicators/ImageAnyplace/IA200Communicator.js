42a2aca6452c648f298c86b02f24e24123706034
chris <chris@digitalworlds.ufl.edu>
2017-04-10 10:34:58 -0400

    IA200 controls added.  SynchronousTCPCommunicator implemented.

--- Communicators/ImageAnyplace/IA200Communicator.js
+++ Communicators/ImageAnyplace/IA200Communicator.js
@@ -5,8 +5,9 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var debugMod = require("debug");
-var TCPCommunicator_1 = require("../TCPCommunicator");
 var TCPCommand_1 = require("../TCPCommand");
+var Control_1 = require("../../shared/Control");
+var SynchronousTCPCommunicator_1 = require("../SynchronousTCPCommunicator");
 var debug = debugMod("comms");
 var IA200Communicator = (function (_super) {
     __extends(IA200Communicator, _super);
@@ -17,50 +18,139 @@ var IA200Communicator = (function (_super) {
         return _this;
     }
     IA200Communicator.prototype.buildCommandList = function () {
-        var nineZeros = "000000000";
-        var fiveZeros = "00000";
-        var inputConfig = {
-            cmdStr: "Video Input",
-            cmdUpdateTemplate: "A00WBA%s" + nineZeros,
+        // Picture Settings
+        this.commands["Brightness"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Brightness", "AAA", 0, 100));
+        this.commands["Contrast"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Contrast", "AAB", 0, 100));
+        this.commands["Sharpness"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Sharpness", "AAC", 0, 100));
+        this.commands["Detail Enhancement"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Detail Enhancement", "AAD", 0, 100));
+        // Input/Output Gamma skipped
+        this.commands["Color"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Color", "AAI", 0, 100));
+        this.commands["Hue"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Hue", "AAJ", 0, 360));
+        // Aspect Ratio
+        this.commands["Aspect Ratio"] = new TCPCommand_1.TCPCommand(this.selectCommandConfig("Aspect Ratio", "AB", {
+            A: "Standard",
+            B: "Full Screen",
+            C: "Crop",
+            D: "Anamorphic",
+            E: "Flexview",
+            F: "TheatreScope",
+            G: "Squeeze"
+        }));
+        // Picture Position
+        this.commands["Vertical Position"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Vertical Position", "ACA", -1, 400));
+        this.commands["Horizontal Position"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Horizontal Position", "ACB", -1280, 1280));
+        // Sync
+        this.commands["Clock"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Clock", "AEA", 0, 200));
+        this.commands["Phase"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Phase", "AEB", 0, 31));
+        // Video Inputs
+        this.commands["Video Input"] = new TCPCommand_1.TCPCommand(this.selectCommandConfig("Video Input", "BA", {
+            A: "Component",
+            B: "RGB",
+            C: "DVI",
+            D: "S-Video",
+            E: "Composite",
+            F: "SDI",
+            G: "HDMI"
+        }));
+        // Info
+        this.commands["Input Resolution"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Input Resolution", "EA"));
+        this.commands["Input H frequency"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Input H frequency", "EB"));
+        this.commands["Input V frequency"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Input V frequency", "EC"));
+        this.commands["Output Mode"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Output Mode", "ED"));
+        this.commands["Output Resolution"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Output Resolution", "EE"));
+        this.commands["Output H frequency"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Output H frequency", "EF"));
+        this.commands["Output F frequency"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Output F frequency", "EG"));
+        this.commands["Sync Mode"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Sync Mode", "EH"));
+        this.commands["Firmware Revision"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Firmware Revision", "EI"));
+        this.commands["Serial Number"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Serial Number", "EJ"));
+        this.commands["IP Address"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("IP Address", "EK"));
+        this.commands["FPGA revision"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("FPGA revision", "EL"));
+        this.commands["Standby Micro Rev. #"] = new TCPCommand_1.TCPCommand(this.infoControlConfig("Standby Micro Rev. #", "EM"));
+        // Keystone
+        this.commands["Keystone TL-H"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone TL-H", "FAA", 0, 650));
+        this.commands["Keystone TL-V"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone TL-V", "FAB", 0, 650));
+        this.commands["Keystone BL-H"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone BL-H", "FAC", 0, 650));
+        this.commands["Keystone BL-V"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone BL-V", "FAD", -650, 0));
+        this.commands["Keystone TR-H"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone TR-H", "FAE", -650, 0));
+        this.commands["Keystone TR-V"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone TR-V", "FAF", 0, 650));
+        this.commands["Keystone BR-H"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone BR-H", "FAG", -650, 0));
+        this.commands["Keystone BR-V"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Keystone BR-V", "FAH", -650, 0));
+        // Advanced/Edge Blending
+        this.commands["Bottom Edge Blending"] = new TCPCommand_1.TCPCommand(this.boolControlConfig("Bottom Edge Blending", "FFA"));
+        this.commands["Left Edge Blending"] = new TCPCommand_1.TCPCommand(this.boolControlConfig("Left Edge Blending", "FFB"));
+        this.commands["Right Edge Blending"] = new TCPCommand_1.TCPCommand(this.boolControlConfig("Right Edge Blending", "FFC"));
+        this.commands["Top Edge Blending"] = new TCPCommand_1.TCPCommand(this.boolControlConfig("Top Edge Blending", "FFD"));
+        this.commands["Bottom Edge Size"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Bottom Edge Size", "FFE", 0, 511));
+        this.commands["Left Edge Size"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Left Edge Size", "FFF", 0, 511));
+        this.commands["Right Edge Size"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Right Edge Size", "FFG", 0, 511));
+        this.commands["Top Edge Size"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Top Edge Size", "FFH", 0, 511));
+        this.commands["Red Brightness"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Red Brightness", "FFJ", 0, 100));
+        this.commands["Green Brightness"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Green Brightness", "FFL", 0, 100));
+        this.commands["Blue Brightness"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Blue Brightness", "FFM", 0, 100));
+        this.commands["Contrast Red"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Contrast Red", "FFN", 0, 100));
+        this.commands["Contrast Green"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Contrast Green", "FFO", 0, 100));
+        this.commands["Contrast Blue"] = new TCPCommand_1.TCPCommand(this.rangeControlConfig("Contrast Blue", "FFP", 0, 100));
+    };
+    IA200Communicator.prototype.boolControlConfig = function (cmdStr, cmdCode) {
+        return {
+            cmdStr: cmdStr,
+            cmdQueryStr: "A00R" + cmdCode,
+            cmdQueryResponseRE: /V0{0,3}([\d-]*)/,
+            cmdUpdateTemplate: "A00W" + cmdCode + "%04d00000",
             cmdUpdateResponseTemplate: "K",
             endpoint_id: this.config.endpoint._id,
-            control_type: "string",
-            usertype: "select",
-            templateConfig: {
-                options: {
-                    A: "Component",
-                    B: "RGB",
-                    C: "DVI",
-                    D: "S-Video",
-                    E: "Composite",
-                    F: "SDI",
-                    G: "HDMI"
-                }
-            },
-            poll: 0,
-            writeonly: true
+            control_type: Control_1.Control.CONTROL_TYPE_BOOLEAN,
+            usertype: Control_1.Control.USERTYPE_SWITCH,
+            poll: 1,
+            templateConfig: {},
+            readonly: true
         };
-        this.commands[inputConfig.cmdStr] = new TCPCommand_1.TCPCommand(inputConfig);
-        var keystoneConfig = {
-            cmdStr: "Keystone TL-X",
-            cmdQueryStr: "A00RFAA",
-            cmdQueryResponseRE: /V0(\d\d\d)/,
-            cmdUpdateTemplate: "A00WFAA%04d" + fiveZeros,
+    };
+    IA200Communicator.prototype.infoControlConfig = function (cmdStr, cmdCode) {
+        return {
+            cmdStr: cmdStr,
+            cmdQueryStr: "A00R" + cmdCode,
+            cmdQueryResponseRE: /v(.*)/,
+            endpoint_id: this.config.endpoint._id,
+            control_type: Control_1.Control.CONTROL_TYPE_STRING,
+            usertype: Control_1.Control.USERTYPE_SELECT_READONLY,
+            poll: 1,
+            templateConfig: {},
+            readonly: true
+        };
+    };
+    IA200Communicator.prototype.rangeControlConfig = function (cmdStr, cmdCode, min, max) {
+        return {
+            cmdStr: cmdStr,
+            cmdQueryStr: "A00R" + cmdCode,
+            cmdQueryResponseRE: /V0{0,3}([\d-]*)/,
+            cmdUpdateTemplate: "A00W" + cmdCode + "%04d00000",
             cmdUpdateResponseTemplate: "K",
             endpoint_id: this.config.endpoint._id,
-            control_type: "range",
-            usertype: "slider",
+            control_type: Control_1.Control.CONTROL_TYPE_RANGE,
+            usertype: Control_1.Control.USERTYPE_SLIDER,
             templateConfig: {
-                min: 0,
-                max: 800
+                min: min,
+                max: max
             },
             poll: 1
         };
-        this.commands[keystoneConfig.cmdStr] = new TCPCommand_1.TCPCommand(keystoneConfig);
-        keystoneConfig.cmdStr = "Keystone TL-V";
-        keystoneConfig.cmdQueryStr = "A00RFAB";
-        keystoneConfig.cmdUpdateTemplate = "A00WFAB%04d" + fiveZeros;
-        this.commands[keystoneConfig.cmdStr] = new TCPCommand_1.TCPCommand(keystoneConfig);
+    };
+    IA200Communicator.prototype.selectCommandConfig = function (cmdStr, cmdCode, options, writeonly) {
+        if (writeonly === void 0) { writeonly = true; }
+        return {
+            cmdStr: cmdStr,
+            cmdUpdateTemplate: "A00W" + cmdCode + "%s000000000",
+            cmdUpdateResponseTemplate: "K",
+            endpoint_id: this.config.endpoint._id,
+            control_type: Control_1.Control.CONTROL_TYPE_STRING,
+            usertype: Control_1.Control.USERTYPE_SELECT,
+            templateConfig: {
+                options: options
+            },
+            poll: 0,
+            writeonly: writeonly
+        };
     };
     // Match one of the IA200 response codes and return it
     IA200Communicator.prototype.matchResponseCode = function () {
@@ -102,10 +192,11 @@ var IA200Communicator = (function (_super) {
         while ((resp = this.matchResponseCode())) {
             this.processLine(resp);
             this.inputBuffer = this.inputBuffer.slice(resp.length);
+            this.runNextCommand();
         }
     };
     return IA200Communicator;
-}(TCPCommunicator_1.TCPCommunicator));
+}(SynchronousTCPCommunicator_1.SynchronousTCPCommunicator));
 var communicator = new IA200Communicator();
 module.exports = communicator;
 //# sourceMappingURL=IA200Communicator.js.map
---
+++ Communicators/SynchronousTCPCommunicator.ts
@@ -0,0 +1,138 @@
+import {TCPCommand} from "./TCPCommand";
+import {TCPCommunicator} from "./TCPCommunicator";
+import {ControlUpdateData} from "../shared/ControlUpdate";
+
+let debug = console.log;
+
+
+export class SynchronousTCPCommunicator extends TCPCommunicator {
+    commandQueue : string[] = [];
+    expectedResponsesQueue: [string | RegExp, (line: string) => any][] = [];
+    commandQueueRunning = false;
+    commandTimeoutTimer : any = 0;
+
+    executeCommandQuery(cmd: TCPCommand) {
+        if (! cmd.queryString()) {
+            return;
+        }
+
+        let self = this;
+        let queryStr = cmd.queryString();
+        debug("queueing query: " + queryStr);
+
+        this.queueCommand(queryStr + this.outputLineTerminator,
+            [
+                cmd.queryResponseMatchString(),
+                (line) => {
+                    for (let ctid of cmd.ctidList) {
+                        let control = self.controlsByCtid[ctid];
+
+                        let val = cmd.parseQueryResponse(control, line);
+                        self.setControlValue(control, val);
+                    }
+
+                    self.connectionConfirmed();
+                }
+            ]
+        );
+    }
+
+
+    handleControlUpdateRequest(request: ControlUpdateData) {
+        if (! this.connected) {
+            return;
+        }
+
+        let control = this.controls[request.control_id];
+        let command = this.commandsByTemplate[control.ctid];
+
+        let updateStr = command.updateString(control, request);
+        debug("sending update: " + updateStr);
+
+        this.queueCommand(updateStr + this.outputLineTerminator,
+            [
+                command.updateResponseMatchString(request),
+                (line) => {
+                    this.setControlValue(control, request.value);
+                }
+            ]
+        );
+
+        // Mark this control as indeterminate, in case we see a query or other update
+        // regarding it but the expected response never comes
+        this.indeterminateControls[request.control_id] = true;
+    }
+
+    onData(data: any) {
+        let strData = '';
+        if (this.commsMode == 'string') {
+            strData = String(data);
+        }
+        else if (this.commsMode == 'hex') {
+            strData = data.toString('hex');
+        }
+
+        debug("data recieved: " + strData);
+
+        this.inputBuffer += strData;
+        let lines = this.inputBuffer.split(this.inputLineTerminator);
+
+        while (lines.length > 1) {
+            this.processLine(lines[0]);
+            lines.splice(0,1);
+            this.runNextCommand();
+        }
+
+        this.inputBuffer = lines[0];
+    }
+
+
+    queueCommand(cmdStr : string, expectedResponse: [string | RegExp, (line: string) => any]) {
+        this.commandQueue.push(cmdStr);
+        this.expectedResponsesQueue.push(expectedResponse);
+
+        if (! this.commandQueueRunning) {
+            this.runCommandQueue();
+        }
+    }
+
+
+    runCommandQueue() {
+        if (this.commandQueueRunning) {
+            return;
+        }
+
+        this.commandQueueRunning = true;
+        this.runNextCommand();
+    }
+
+    runNextCommand() {
+        // If we are out of commands, reset the queues and stop
+        if (this.commandTimeoutTimer) {
+            clearTimeout(this.commandTimeoutTimer);
+        }
+        this.commandTimeoutTimer = 0;
+
+        if (this.commandQueue.length == 0 || this.expectedResponsesQueue.length == 0) {
+            this.commandQueueRunning = false;
+            this.commandQueue = [];
+            this.expectedResponsesQueue = [];
+            return;
+        }
+
+        let command = this.commandQueue[0];
+        let expectedResponse = this.expectedResponsesQueue[0];
+
+        this.commandQueue.splice(0, 1);
+        this.expectedResponsesQueue.splice(0,1);
+
+        this.expectedResponses = [expectedResponse];
+
+
+        let logCommand = command.replace(/\r?\n|\r/g, '');
+        debug("sending command: " + logCommand);
+        this.writeToSocket(command);
+
+        this.commandTimeoutTimer = setTimeout(this.runNextCommand, 400);
+    }
+}

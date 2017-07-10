d29d6c3c8aea520db65d5fc759d3abc266a45f41
chris <chris@digitalworlds.ufl.edu>
2017-07-10 11:04:40 -0400

    Changed config.js to typescript

--- server/config.js
+++ server/config.js
@@ -1,51 +1,35 @@
 var fs = require('fs');
-
 var config = {
-  wsUrl : "https://devctrl.dwi.ufl.edu/",
-  testString : "default",
-  endpointId : "overrideme",
-  mongoHost : "localhost",
-  mongoPort : 27017,
-  mongoDB : "devctrl",
-  ioPort : 2880,
-  ioPath: "/socket.io",
-  authPort : 2992,
-  "endpointPassword" : "DWCONTROL",
-  app: "./ncontrol",
-  authId: "overrideme",
-  identifierName : "identifier"
+    wsUrl: "https://devctrl.dwi.ufl.edu/",
+    testString: "default",
+    endpointId: "overrideme",
+    mongoHost: "localhost",
+    mongoPort: 27017,
+    mongoDB: "devctrl",
+    ioPort: 2880,
+    ioPath: "/socket.io",
+    authPort: 2992,
+    "endpointPassword": "DWCONTROL",
+    app: "./ncontrol",
+    authId: "overrideme",
+    identifierName: "identifier"
 };
-
 var localConfig = {};
 var localPath = "./config.local.js";
-
 if (fs.existsSync(localPath)) {
-  localConfig = require("./config.local");
+    localConfig = require("./config.local");
 }
-
 for (var opt in localConfig) {
-  config[opt] = localConfig[opt];
+    config[opt] = localConfig[opt];
 }
-
 var customConfig = {};
-
 if (typeof process.argv[2] !== 'undefined') {
-
-  console.log("arg 2 is " + process.argv[2]);
-  var configArg = process.argv[2];
-  var configPath = "./conf/" + configArg + ".js";
-
-  if (fs.existsSync(configPath)) {
-    customConfig = require("./conf/" + configArg);
-  }
+    console.log("arg 2 is " + process.argv[2]);
+    var configArg = process.argv[2];
+    var configPath = "./conf/" + configArg + ".js";
+    if (fs.existsSync(configPath)) {
+        console.log(loading, $, (_a = [");\n    customConfig = require(\"./conf/\" + configArg);\n  }\n  else {\n    console.log("], _a.raw = [");\n    customConfig = require(\"./conf/\" + configArg);\n  }\n  else {\n    console.log("], { configPath: configPath }(_a)), $, { configPath: configPath }, not, (_b = [");\n  }\n}\n\nfor (opt in customConfig) {\n    config[opt] = customConfig[opt];\n}\n\nif (typeof process.argv[3] !== 'undefined') {\n  config.endpointId = process.argv[3];\n}\n\nmodule.exports =  config;"], _b.raw = [");\n  }\n}\n\nfor (opt in customConfig) {\n    config[opt] = customConfig[opt];\n}\n\nif (typeof process.argv[3] !== 'undefined') {\n  config.endpointId = process.argv[3];\n}\n\nmodule.exports =  config"], found(_b)));
+    }
 }
-
-for (opt in customConfig) {
-    config[opt] = customConfig[opt];
-}
-
-if (typeof process.argv[3] !== 'undefined') {
-  config.endpointId = process.argv[3];
-}
-
-module.exports =  config;
+var _a, _b;
+//# sourceMappingURL=config.js.map

c79fa4b0485a095ecdfef16b64ee36f6d2f17007
chris <chris@digitalworlds.ufl.edu>
2017-08-24 15:39:26 -0400

    Add input select command to Christie ESeries

--- Communicators/Christie/ESeriesCommunicator.ts
+++ Communicators/Christie/ESeriesCommunicator.ts
@@ -15,6 +15,7 @@ class ESeriesCommunicator extends SynchronousTCPCommunicator {
         this.registerSwitchCommand("Power", "PWR");
         this.registerRangeCommand("HORZ Position", "HOR", 0, 100);
         this.registerRangeCommand("VERT Position", "VRT", 0, 100);
+        this.registerInputSelectCommand();
     }
 
     registerRangeCommand(name: string, cmd: string, min: number, max: number) {
@@ -33,6 +34,33 @@ class ESeriesCommunicator extends SynchronousTCPCommunicator {
                 max: max
             }
         })
+    }
+
+
+    registerInputSelectCommand() {
+        let cmd = "SIN";
+        this.commands["Input/Source"] = new TCPCommand({
+            cmdStr: "Input/Source",
+            cmdQueryStr: `(${cmd}?)`,
+            cmdQueryResponseRE: `\\(${cmd}\\!(\\d+)\\)`,  // eg. (PWR!1)
+            cmdUpdateTemplate: `(#${cmd}%i)`,
+            cmdUpdateResponseTemplate: `(${cmd}!%i)`,
+            endpoint_id: this.endpoint_id,
+            control_type: Control.CONTROL_TYPE_STRING,
+            usertype: Control.USERTYPE_SELECT,
+            poll: 1,
+            templateConfig: {
+                options : {
+                    1 : "VGA",
+                    2 : "BNC",
+                    3 : "HDMI 1",
+                    4 : "HDMI 2",
+                    7 : "Component",
+                    8 : "S-Video",
+                    9 : "Composite"
+                }
+            }
+        });
     }
 
     registerSwitchCommand(name: string, cmd: string) {
